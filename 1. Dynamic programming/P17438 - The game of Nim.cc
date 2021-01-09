726cacae62e17b8d100ec485d7b9a9139486606b
Wind-yk <58819697+Wind-yk@users.noreply.github.com>
2021-01-09 15:39:20 +0100

    Update P17438 - The game of Nim.cc

--- 1. Dynamic programming/P17438 - The game of Nim.cc
+++ 1. Dynamic programming/P17438 - The game of Nim.cc
@@ -1,48 +1,39 @@
-#include <bits/stdc++.h>
-
+#include <iostream>
+#include <vector>
+#include <map>
 using namespace std;
 
-int dp[2][3][4][5][6][7];
+map<vector<int>, bool> M;
 
-int fun(int x1, int x2, int x3, int x4, int x5, int x6) {
-    if (dp[x1][x2][x3][x4][x5][x6]!=-1)
-        return dp[x1][x2][x3][x4][x5][x6];
-    for (int i=1; i<=x1; ++i)
-        if (!fun(x1-i, x2, x3, x4, x5, x6))
-            return dp[x1][x2][x3][x4][x5][x6]=1;
-    for (int i=1; i<=x2; ++i)
-        if (!fun(x1, x2-i, x3, x4, x5, x6))
-            return dp[x1][x2][x3][x4][x5][x6]=1;
-    for (int i=1; i<=x3; ++i)
-        if (!fun(x1, x2, x3-i, x4, x5, x6))
-            return dp[x1][x2][x3][x4][x5][x6]=1;
-    for (int i=1; i<=x4; ++i)
-        if (!fun(x1, x2, x3, x4-i, x5, x6))
-            return dp[x1][x2][x3][x4][x5][x6]=1;
-    for (int i=1; i<=x5; ++i)
-        if (!fun(x1, x2, x3, x4, x5-i, x6))
-            return dp[x1][x2][x3][x4][x5][x6]=1;
-    for (int i=1; i<=x6; ++i)
-        if (!fun(x1, x2, x3, x4, x5, x6-i))
-            return dp[x1][x2][x3][x4][x5][x6]=1;
-    return dp[x1][x2][x3][x4][x5][x6]=0;
+// Diu si el jugador que rep aquest configuracio guanya
+bool winning(vector<int>& conf) {
+    if(M.find(conf) != M.end()) return M[conf];
+
+    int s = 0;
+    for(auto& x: conf) s +=x;
+    if(s == 0) return M[conf] = false; // Tot zero --> perdedora
+
+    for(uint pos = 0; pos < conf.size(); ++pos) {
+        for(int t = 1; t <= conf[pos]; ++t){
+            // El moviment consta en treure "t" peces de la posició pos
+            conf[pos] -= t;
+            if(not winning(conf)) { conf[pos] += t; return M[conf] = true; }
+            conf[pos] += t;
+            // Observa que hem de sumar tot i que anem a retornar,
+            // ja que és possible que usem el valor de conf[pos] de forma recursiva.
+        }
+    }
+    return M[conf] = false;
 }
 
 int main() {
-    memset(dp, -1, 7*6*5*4*3*2*4);
-    dp[0][0][0][0][0][0]=0;
-    int t;
-    string a, b;
-    int p1, p2, p3, p4, p5, p6;
-    cin>>t;
-    for (int i=1; i<=t; ++i) {
-        cin>>a>>b;
-        cin>>p1>>p2>>p3>>p4>>p5>>p6;
-        cout<<"Game #"<<i<<": ";
-        if (fun(p1, p2, p3, p4, p5, p6))
-            cout<<a;
-        else
-            cout<<b;
-        cout<<endl;
+    int n;
+    cin >> n;
+    string jug1, jug2;
+    vector<int> conf(6);
+    for (int i = 1; i <= n; ++i) {
+        cin >> jug1 >> jug2;
+        for(int& c : conf) cin >> c;
+        cout << "Game #" << i << ": " << (winning(conf) ? jug1:jug2) << endl;
     }
 }
